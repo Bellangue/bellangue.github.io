669125be91a70d74719e3aa2871820137f76c901
ollyturner <oeturner1@outlook.com>
2023-10-09 20:18:39 +0200

    Added snails

--- speaking/js/frLessonData.js
+++ speaking/js/frLessonData.js
@@ -324,5 +324,5 @@ if (localStorage.getItem("currentLanguage") === "fr") {
 
 
 
-    lessonJSONData = [lesson1Json, lesson2Json, lesson3Json, lesson4Json, lesson5Json, lesson6Json, lesson7Json, lesson8Json, lesson9Json, lesson10Json, lesson11Json, lesson12Json, lesson13Json];
+    lessonJSONData = [lesson1Json, lesson2Json, lesson3Json, lesson4Json, lesson5Json, lesson6Json, lesson7Json, lesson8Json, lesson9Json, lesson10Json, lesson11Json, lesson12Json, lesson13Json, lesson14Json, lesson15Json, lesson16Json, lesson17Json, lesson18Json, lesson19Json, lesson20Json, lesson21Json, lesson22Json, lesson23Json, lesson24Json, lesson25Json, lesson26Json, lesson27Json, lesson28Json, lesson29Json, lesson30Json];
 }
