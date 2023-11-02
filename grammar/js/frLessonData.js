e4b7e0945d399aea201c8e09d972607b868a21ab
ollyturner <oeturner1@outlook.com>
2023-11-02 19:11:18 +0100

    Added hintsGrammar

--- grammar/js/frLessonData.js
+++ grammar/js/frLessonData.js
@@ -4,7 +4,7 @@ if (localStorage.getItem("currentLanguage") === "fr") {
 
     lesson1Json = [
         {
-            "Rule": "Before a vowel or <h>, certain words, like \"Je\" get condensed. (Je + ai = J'ai)",
+            "Rule": "Before a vowel or 'h', certain words, like 'Je' get condensed. (Je + ai = J'ai)",
             "Question": "Je ___ (me + appelle) Pierre.",
             "Answers": ["m'appelle"],
             "Options": ["m'appelle",
@@ -15,5 +15,5 @@ if (localStorage.getItem("currentLanguage") === "fr") {
         },
     ]
 
-    lessonJSONData = [lesson1Json, lesson2Json, lesson3Json, lesson4Json, lesson5Json, lesson6Json, lesson7Json, lesson8Json, lesson9Json, lesson10Json, lesson11Json, lesson12Json, lesson13Json, lesson14Json, lesson15Json, lesson16Json, lesson17Json, lesson18Json, lesson19Json, lesson20Json, lesson21Json, lesson22Json, lesson23Json, lesson24Json, lesson25Json, lesson26Json, lesson27Json, lesson28Json, lesson29Json, lesson30Json];
+    lessonJSONData = [lesson1Json];
 }
